94bbfb0548e77756c13ee5572eae67b7c578ab41
liuxin <963875368@qq.com>
2026-01-31 16:42:46 +0800

    list学习

--- src/core/ngx_list.c
+++ src/core/ngx_list.c
@@ -8,56 +8,64 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+/**
+ * pool 内存池对象
+ * n 每个链表元素数组可以容纳多少个个数
+ * size 每个元素的大小
+ */
+ 
 ngx_list_t *
 ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
     ngx_list_t  *list;
 
     list = ngx_palloc(pool, sizeof(ngx_list_t));
-    if (list == NULL) {
+    if (list == NULL) {// 内存分配失败
         return NULL;
     }
 
-    if (ngx_list_init(list, pool, n, size) != NGX_OK) {
+    if (ngx_list_init(list, pool, n, size) != NGX_OK) {// 链表初始化失败
         return NULL;
     }
 
     return list;
 }
 
-
+/**
+ * null 失败
+ * 返回到是节点里面元素数组开始的存储位置
+ */
 void *
 ngx_list_push(ngx_list_t *l)
 {
     void             *elt;
     ngx_list_part_t  *last;
-
+    // list最有一个节点
     last = l->last;
 
-    if (last->nelts == l->nalloc) {
+    if (last->nelts == l->nalloc) {  // nelts==nalloc 节点里面的数组元素存储满了
 
         /* the last part is full, allocate a new list part */
 
         last = ngx_palloc(l->pool, sizeof(ngx_list_part_t));
-        if (last == NULL) {
+        if (last == NULL) {// 内存分配失败 返回NULL
             return NULL;
         }
-
+        // 分配节点里面元素数组空间
         last->elts = ngx_palloc(l->pool, l->nalloc * l->size);
-        if (last->elts == NULL) {
+        if (last->elts == NULL) {// 分配内存错误
             return NULL;
         }
-
+        // 当前节点里面数组 有0个元素
         last->nelts = 0;
         last->next = NULL;
-
+        // 将新申请的节点插入到最后
         l->last->next = last;
         l->last = last;
     }
 
     elt = (char *) last->elts + l->size * last->nelts;
     last->nelts++;
-
+    // 返回节点元素存储的位置 并将nelts ++ 
     return elt;
 }
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -12,22 +12,23 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 
-
+// ngx_list_part_t 链表的一个元素(节点)  （实际上是一个固定大小的数组块）
 typedef struct ngx_list_part_s  ngx_list_part_t;
 
+// 链表元素 这样设计非常灵活 可以是任何一种数据结构
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
-    ngx_list_part_t  *next;
+    void             *elts;// 指向该节点的数据区（数组首地址）
+    ngx_uint_t        nelts;//该节点已使用的元素个数
+    ngx_list_part_t  *next;// 下一个链表节点
 };
 
 
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
-    size_t            size;
-    ngx_uint_t        nalloc;
-    ngx_pool_t       *pool;
+    ngx_list_part_t  *last;// 指向链表的最后一个元素
+    ngx_list_part_t   part;// 链表的第一个节点（直接内嵌在头里，减少一次分配）
+    size_t            size;// 单个元素的大小 ngx_list_part_s.elts 占用大小
+    ngx_uint_t        nalloc;// 每个节点（数组块）能容纳的元素总数
+    ngx_pool_t       *pool;// 内存池（用于分配新节点）
 } ngx_list_t;
 
 
@@ -36,16 +37,22 @@ ngx_list_t *ngx_list_create(ngx_pool_t *pool, ngx_uint_t n, size_t size);
 static ngx_inline ngx_int_t
 ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 {
+    //链表第一个节点里面 elts 申请内存
     list->part.elts = ngx_palloc(pool, n * size);
     if (list->part.elts == NULL) {
         return NGX_ERROR;
     }
-
+    //数组存储了0个元素
     list->part.nelts = 0;
+    // 下一个节点指向位置
     list->part.next = NULL;
+    // 当前既是第一个元素也是最后一个 设置最后一个位置
     list->last = &list->part;
+    //设置每个节点上数组存储元素占用大小
     list->size = size;
+    // 每个节点存储元素个数
     list->nalloc = n;
+    // 内存池指针
     list->pool = pool;
 
     return NGX_OK;
@@ -53,24 +60,26 @@ ngx_list_init(ngx_list_t *list, ngx_pool_t *pool, ngx_uint_t n, size_t size)
 
 
 /*
- *
+ *  遍历列表
  *  the iteration through the list:
  *
- *  part = &list.part;
- *  data = part->elts;
+ *  part = &list.part;// 第一个节点地址
+ *  data = part->elts;// 第一个节点数组存在几个元素
  *
  *  for (i = 0 ;; i++) {
- *
+ *      // 如果i 超过了节点里面存储元素个数 
+ *      // 设置下一个节点状态
  *      if (i >= part->nelts) {
  *          if (part->next == NULL) {
- *              break;
+ *              break;// 不存在下一个节点退出循环
  *          }
- *
- *          part = part->next;
- *          data = part->elts;
- *          i = 0;
+ *          
+ *          part = part->next;// 下一个节点
+ *          data = part->elts;//下一个节点元素数组的位置
+ *          i = 0;// 重置节点数组的索引
  *      }
- *
+ *      
+ *      // 获取遍历的值
  *      ...  data[i] ...
  *
  *  }
